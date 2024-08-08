5d3a00d945aa682e957df4e473c2dc85f1edc98d
d.morozov <d.morozov@artjoker.team>
2024-08-08 23:01:10 +0300

    add authProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { Admin, Resource } from 'react-admin';
 import posts from './posts';
 import comments from './comments';
 import dataProvider from './dataProvider.ts'
+import authProvider from './authProvider.ts';
 
 function App() {
 
   return (
-    <Admin dataProvider={dataProvider}>
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
       <Resource name="posts" {...posts} />
       <Resource name='comments' {...comments} />
     </Admin>
